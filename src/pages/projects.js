3028552a3f256fd30abdc4c5886d1f17cccd725f
SanketJadhav7d3 <lonewolf7d3@gmail.com>
2022-11-25 20:41:51 +0530

    Projects page complete

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,13 +2,29 @@
 import React from 'react';
 
 import './projects.css';
+import GameOfLifeImg from './images/gameoflife.png';
+import AtariImg from './images/atari.png';
+
 
 function Project(props) {
+
+    function openLink(url) {
+        window.open(url, '_blank', 'noopener')
+    }
+
     return (
-        <div className="project">
-            <img src={require("./images/atari.png")} />
+        <div className="project" onClick={() => openLink(props.link)}>
+            <img src={props.image} />
             <div className="project-description">
-                {props.description}
+                <div className="project-title">
+                    {props.name}
+                </div>
+                <div className="technologies">
+                    {props.technologies}
+                </div>
+                <div className="project-tools">
+                    {props.tools}
+                </div>
             </div>
         </div>
     );
@@ -17,8 +33,20 @@ function Project(props) {
 export default function Projects() {
     return (
         <div className="projects-container">
-            <Project name="What" description="Trained an AI to play Atari breakout" image="./images/atari.png"/>
-            <Project name="What" image="./images/atari.png"/>
+            <Project 
+                name="Trained an AI to play Atari breakout" 
+                technologies="Reinforcement Learning"
+                image={AtariImg}
+                tools="Python | NEAT Algorithm | Pygame"
+                link="https://github.com/SanketJadhav7d3/AI-plays-Atari-Breakout.git"
+            />
+            <Project 
+                name="Game of life using pygame" 
+                technologies="Algorithms"
+                image={GameOfLifeImg}
+                tools="Python | Pygame"
+                link="https://github.com/SanketJadhav7d3/game-of-life-pygame.git"
+            />
         </div>
     );
 }
