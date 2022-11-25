7469af2c921a1ecaf109c575ad7ae9d7648cc0c4
SanketJadhav7d3 <lonewolf7d3@gmail.com>
2022-11-25 19:17:23 +0530

    Updates

---
+++ precache-manifest.2ee17ab71838f1ca6b3b73b485f664f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "84bb815882f9ff54f12c",
+    "url": "/portfolio-website/static/css/main.5a4fc1e8.chunk.css"
+  },
+  {
+    "revision": "84bb815882f9ff54f12c",
+    "url": "/portfolio-website/static/js/main.84bb8158.chunk.js"
+  },
+  {
+    "revision": "96b947ccf251ce7d847a",
+    "url": "/portfolio-website/static/js/1.96b947cc.chunk.js"
+  },
+  {
+    "revision": "2b256896c0c48cf1d86c",
+    "url": "/portfolio-website/static/js/runtime~main.2b256896.js"
+  },
+  {
+    "revision": "1e837d0cc092c23dd8fc1dd62b020f5e",
+    "url": "/portfolio-website/static/media/background.1e837d0c.jpg"
+  },
+  {
+    "revision": "6891747bab27e85374a7ba74b6a027a7",
+    "url": "/portfolio-website/static/media/atari.6891747b.png"
+  },
+  {
+    "revision": "e9305bd434150f0f6eaab05cc8c7212c",
+    "url": "/portfolio-website/index.html"
+  }
+];
