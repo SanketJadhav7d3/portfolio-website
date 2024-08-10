f4ebeac4fe7a7d9f5fd9f62dcce819a079752cf6
sanketjadhav7d3 <sanketjadhav7d3>
2024-08-10 15:30:41 +0530

    brain mri with prediction results

--- ProjectDescription/brainMRI.js
+++ ProjectDescription/brainMRI.js
@@ -16,6 +16,7 @@ Trained three different architecture for the task of brain MRI Segmentation.
 
 <img src="./ProjectDescription/BrainMRI/data_grid.png" alt="Not found" width="700"><br>
 
+The dataset used in this research includes brain MRI scans and manual segmentation masks of FLAIR abnormality regions from 110 lower-grade glioma patients, sourced from The Cancer Imaging Archive (TCIA) and associated with studies by Buda et al. (2019) and Mazurowski et al. (2017). The dataset features FLAIR imaging sequences and manual segmentation masks as ground truth annotations. Input image dimensions are (batch size, 3, 256, 256), with output masks of (batch size, 1, 256, 256).
 
 
 # UNet
@@ -28,11 +29,18 @@ Trained three different architecture for the task of brain MRI Segmentation.
 <img src="./ProjectDescription/BrainMRI/DeepLabV3/train_valid loss.png" alt="Not found" width="700"><br>
 <img src="./ProjectDescription/BrainMRI/DeepLabV3/train_valid iou.png" alt="Not found" width="700"><br>
 
+## Prediction result
+
+<img src="./ProjectDescription/BrainMRI/DeepLabV3/result.png" alt="Not found" width="700"><br>
+
 # DeepLabV3+
 
 <img src="./ProjectDescription/BrainMRI/DeepLabV3+/train_valid loss.png" alt="Not found" width="700"><br>
 <img src="./ProjectDescription/BrainMRI/DeepLabV3+/train_valid iou.png" alt="Not found" width="700"><br>
 
+## Prediction result
+
+<img src="./ProjectDescription/BrainMRI/DeepLabV3+/result.png" alt="Not found" width="700"><br>
 `;
 
 export default brainMRIContent; 
