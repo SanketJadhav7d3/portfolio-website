9face81ad826d52eb1e93b4811db31b1d1ac4e02
sanketjadhav7d3 <sanketjadhav7d3>
2024-08-10 16:46:17 +0530

    ai plant pathologist video added

--- ProjectDescription/plantDiseaseClassifier.js
+++ ProjectDescription/plantDiseaseClassifier.js
@@ -3,27 +3,16 @@ const plantDiseaseContent = `
 
 # AI Plant Pathologist
 
-This repository contains a deep learning classifier trained on the [Plant Leaf Disease Dataset](https://www.kaggle.com/datasets/vipoooool/new-plant-diseases-dataset) consisting of 38 classes of plant leaf diseases. 
 
-## Model 
+<img src="./ProjectDescription/ai-plant-pathologist/interface-image-1.png" alt="Not found" width="700"><br>
+<img src="./ProjectDescription/ai-plant-pathologist/interface-image-2.png" alt="Not found" width="700"><br>
 
-A pre-trained Resnet-50 model was fine-tuned on the Plant Leaf Disease Dataset.
+# Demo
 
-## Deployment
-
-* The trained classifier has been deployed to the Google Vertex AI Endpoint. 
-* The flask frontend is hosted on google cloud run.
-* Google cloud storage is also used to store the uploaded images.
-
-Click here to try
-
-https://classifier-guzxsmcyia-el.a.run.app (currently doesn't work)
-
-# Tensorflow savedmodel format drive link
-
-https://drive.google.com/drive/folders/1--JgkNaDHVVI2RY5CscyuZ7C-upJxsu0?usp=sharing
-
-# To run the flask app locally switch to local branch
+<video width="700" controls>
+  <source src="./ProjectDescription/ai-plant-pathologist/AI-Plant-Pathologist.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
 `;
 
 export default plantDiseaseContent;
