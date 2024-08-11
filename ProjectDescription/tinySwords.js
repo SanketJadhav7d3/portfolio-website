6a462bb9912e78f3f08f880c5b079c324c17bb57
sanketjadhav7d3 <sanketjadhav7d3>
2024-08-11 13:15:02 +0530

    added tinyswords images and video

--- ProjectDescription/tinySwords.js
+++ ProjectDescription/tinySwords.js
@@ -10,6 +10,13 @@ https://github.com/SanketJadhav7d3/2D-Platformer
 
 # You can see the idea here
 
+<img src="./ProjectDescription/TinySwords/demo-img-1.png" alt="Not found" width="700"><br>
+
+<video width="700" controls>
+  <source src="./ProjectDescription/TinySwords/demo.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+
 https://sanketjadhav7d3.github.io/tiny-swords-prototype/
 `;
 
